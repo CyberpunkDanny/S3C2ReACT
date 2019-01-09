bd9ac56c2db00cd7622b348be48845a2995e89a8
Divyateja <divyatejadanny@gmail.com>
2019-01-10 01:37:03 +0530

    Combining Reducers
    Splitting a Reducer into multiple simple reducers and then combining them together later using combineReducers()

--- React/confusion/src/redux/configureStore.js
+++ React/confusion/src/redux/configureStore.js
@@ -1,9 +1,20 @@
 /* Store configuration can be done in a separate file like this and then be exported from here */
 
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
+/* To combine simpler reducer functions together, Redux provides a method called combineReducers*/
 export const ConfigureStore = ()=>{
-    const store = createStore(Reducer, initialState);
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+    );
     return store;
 }
